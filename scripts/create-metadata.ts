c731261ea20991fee274bdc8e8578592bd73dce3
root <newbsol@proton.me>
2025-07-27 19:52:15 +0000

    Added NEWB configuration.

---
+++ scripts/create-metadata.ts
@@ -0,0 +1,82 @@
+import {
+  Transaction,
+  PublicKey,
+  Keypair,
+  Connection,
+  clusterApiUrl,
+  sendAndConfirmTransaction,
+} from "@solana/web3.js";
+import fs from "fs";
+import { createCreateMetadataAccountV3Instruction, PROGRAM_ID } from '@metaplex-foundation/mpl-token-metadata';
+
+let connection = new Connection(clusterApiUrl("mainnet-beta"), "confirmed");
+
+async function createMetadata({
+  publicKey,
+  tokenMint,
+  tokenName,
+  tokenSymbol,
+  tokenUrl,
+}: {
+  publicKey: PublicKey;
+  tokenMint: string;
+  tokenName: string;
+  tokenSymbol: string;
+  tokenUrl: string;
+}) {
+  const mint = new PublicKey(tokenMint);
+
+      const createMetadataInstruction = createCreateMetadataAccountV3Instruction(
+        {
+          metadata: PublicKey.findProgramAddressSync(
+            [
+              Buffer.from("metadata"),
+              PROGRAM_ID.toBuffer(),
+              mint.toBuffer(),
+            ],
+            PROGRAM_ID,
+          )[0],
+          mint: mint,
+          mintAuthority: publicKey,
+          payer: publicKey,
+          updateAuthority: publicKey,
+        },
+        {
+          createMetadataAccountArgsV3: {
+            data: {
+              name: tokenName,
+              symbol: tokenSymbol,
+              uri: tokenUrl,
+              creators: null,
+              sellerFeeBasisPoints: 0,
+              uses: null,
+              collection: null,
+            },
+            isMutable: false,
+            collectionDetails: null,
+          },
+        },
+      );
+
+  const updateMetadataTransaction = new Transaction().add(
+    createMetadataInstruction
+  );
+  await sendAndConfirmTransaction(connection, updateMetadataTransaction, [
+    keypair,
+  ]);
+}
+
+const privateKeyFile = fs.readFileSync(
+  "my-keypair0.json"
+);
+let privateKeySeed = JSON.parse(privateKeyFile.toString()).slice(0, 32);
+let keypair = Keypair.fromSeed(Uint8Array.from(privateKeySeed));
+console.log("Token update authority:", keypair.publicKey.toString());
+
+createMetadata({
+  publicKey: keypair.publicKey,
+  tokenMint: "Dg2pYxYHedJaznYG8WJRXwqZwED6WgWafjerUjr4Veky",
+  tokenName: "Newbie",
+  tokenSymbol: "NEWB",
+  tokenUrl: "https://arweave.net/92qtbcsHw5cXDrNQuKnw2Rh_vClsM8EpfI6WNcK4Yjk",
+});
